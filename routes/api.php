f35b267effe976995a8e8f986282d5d2df2d9f14
Viacheslav Kopaev <viacheslav.kopaev@yandex.ru>
2025-04-14 01:53:25 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,78 +1,80 @@
 <?php
 
+use Chatify\Http\Controllers\Api\MessagesController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix(config('chatify.api_routes.prefix'))->middleware(config('chatify.api_routes.middleware'))->group(function () {
+Route::name('api.')->prefix(config('chatify.api_routes.prefix'))->middleware(config('chatify.api_routes.middleware'))->group(function () {
     /**
      * Authentication for pusher private channels
      */
-    Route::post('/chat/auth', 'MessagesController@pusherAuth')->name('api.pusher.auth');
+    Route::post('/chat/auth', [MessagesController::class, 'pusherAuth'])->name('pusher.auth');
 
     /**
-     *  Fetch info for specific id [user/group]
+     * Fetch info for specific id [user/group]
      */
-    Route::post('/idInfo', 'MessagesController@idFetchData')->name('api.idInfo');
+    Route::post('/idInfo', [MessagesController::class, 'idFetchData'])->name('idInfo');
 
     /**
      * Send message route
      */
-    Route::post('/sendMessage', 'MessagesController@send')->name('api.send.message');
+    Route::post('/sendMessage', [MessagesController::class, 'send'])->name('send.message');
 
     /**
      * Fetch messages
      */
-    Route::post('/fetchMessages', 'MessagesController@fetch')->name('api.fetch.messages');
+    Route::post('/fetchMessages', [MessagesController::class, 'fetch'])->name('fetch.messages');
 
     /**
-     * Download attachments route to create a downloadable links
+     * Download attachments route to create downloadable links
      */
-    Route::get('/download/{fileName}', 'MessagesController@download')->name('api.' . config('chatify.attachments.download_route_name'));
+    Route::get('/download/{fileName}', [MessagesController::class, 'download'])
+        ->name(config('chatify.attachments.download_route_name'));
 
     /**
      * Make messages as seen
      */
-    Route::post('/makeSeen', 'MessagesController@seen')->name('api.messages.seen');
+    Route::post('/makeSeen', [MessagesController::class, 'seen'])->name('messages.seen');
 
     /**
      * Get contacts
      */
-    Route::get('/getContacts', 'MessagesController@getContacts')->name('api.contacts.get');
+    Route::get('/getContacts', [MessagesController::class, 'getContacts'])->name('contacts.get');
 
     /**
      * Star in favorite list
      */
-    Route::post('/star', 'MessagesController@favorite')->name('api.star');
+    Route::post('/star', [MessagesController::class, 'favorite'])->name('star');
 
     /**
-     * get favorites list
+     * Get favorites list
      */
-    Route::post('/favorites', 'MessagesController@getFavorites')->name('api.favorites');
+    Route::post('/favorites', [MessagesController::class, 'getFavorites'])->name('favorites');
 
     /**
      * Search in messenger
      */
-    Route::get('/search', 'MessagesController@search')->name('api.search');
+    Route::get('/search', [MessagesController::class, 'search'])->name('search');
 
     /**
      * Get shared photos
      */
-    Route::post('/shared', 'MessagesController@sharedPhotos')->name('api.shared');
+    Route::post('/shared', [MessagesController::class, 'sharedPhotos'])->name('shared');
 
     /**
      * Delete Conversation
      */
-    Route::post('/deleteConversation', 'MessagesController@deleteConversation')->name('api.conversation.delete');
+    Route::post('/deleteConversation', [MessagesController::class, 'deleteConversation'])->name('conversation.delete');
 
     /**
-     * Delete Conversation
+     * Update settings
      */
-    Route::post('/updateSettings', 'MessagesController@updateSettings')->name('api.avatar.update');
+    Route::post('/updateSettings', [MessagesController::class, 'updateSettings'])->name('avatar.update');
 
     /**
      * Set active status
      */
-    Route::post('/setActiveStatus', 'MessagesController@setActiveStatus')->name('api.activeStatus.set');
+    Route::post('/setActiveStatus', [MessagesController::class, 'setActiveStatus'])->name('activeStatus.set');
 
 
 });
